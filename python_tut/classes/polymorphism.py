92d3502dbcfecc6a7b4d55ad33497fd8fc6b8818
Josh Borthick <jborthick019919@gmail.com>
2025-06-06 20:59:56 -0500

    Added examples of polymorphism and duck typing

--- python_tut/classes/polymorphism.py
+++ python_tut/classes/polymorphism.py
@@ -3,7 +3,14 @@
 Author: Josh Borthick
 Copying: Mosh Hamedani
 
+Polymorphism is very powerful.  The second draw function does not need
+to know the type of the control to draw, it just needs passed the list.
 
+It's the same concept for Python's "duck typing" functionality.  If the
+UIControl abstract class were to be removed, the draw function would 
+still work fine, because Python doesn't check type even between the
+different items of the same list.  They can be completely different 
+types or objects and it will run, just fine.
 """
 
 from abc import ABC, abstractmethod
@@ -24,7 +31,22 @@ class DropDownList(UIControl):
         print("Drawing DropDownList")
 
 
+# def draw(control):
+#     """Original draw function."""
+#     control.draw()
+
+
+def draw(controls):
+    """List draw."""
+    for control in controls:
+        control.draw()
+
+
 ddl = DropDownList()
+tb = TextBox()
 print(isinstance(ddl, DropDownList))
 print(isinstance(ddl, UIControl))
-ddl.draw()
+# ddl.draw()
+# draw(ddl)
+
+draw([ddl, tb])
